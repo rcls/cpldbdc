1eb079ca7b4153def45acdff50df0c71760516bb
Ralph Loader <suckfish@ihug.co.nz>
2010-05-22 05:19:32 +1200

    Break support

--- bdc.vhd
+++ bdc.vhd
@@ -10,25 +10,26 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity bdc is
   Port (DQ : inout STD_LOGIC_VECTOR (7 downto 0);
-        RXFinv : in  STD_LOGIC;
-        RDinv : out  STD_LOGIC;
+        RXFi : in  STD_LOGIC;
+        RDi : out  STD_LOGIC;
         WR : out STD_LOGIC;
         BDC : inout  STD_LOGIC := 'Z';
         Clk : in std_logic);
 end bdc;
 
 architecture Behavioral of bdc is
-  signal count : std_logic_vector (3 downto 0) := "0000";
-  signal data : std_logic_vector (4 downto 0) := "00000";
+  signal count : std_logic_vector (3 downto 0) := x"0";
+  signal counthi : std_logic_vector (7 downto 0) := x"00";
+  signal data : std_logic_vector (3 downto 0) := x"0";
 
-  type state_t is (send_bits, read_bits, long_break, idle);
+  type state_t is (send_bits, read_bits, long_break,
+                   read_break, acknowledge, idle);
 
   signal state : state_t := idle;
 
-  signal RDinvInt : std_logic := '1';
+  signal RDiInt : std_logic := '1';
   signal WRint : std_logic := '0';
 
-  signal BDCprev : std_logic;
   -- 0,1,2,3 - send start pulse.
   -- 4 - drive high for 1.
   -- 10 - sample.
@@ -37,31 +38,34 @@ architecture Behavioral of bdc is
   -- 15 - read next command.
 
 begin
-  RDinv <= RDinvInt;
+  RDi <= RDiInt;
   WR <= WRint;
 
-  DQ <= "000" & data when WRint = '1' else "ZZZZZZZZ";
+  DQ <= "0011" & data when WRint = '1' else "ZZZZZZZZ";
 
   process (Clk)
     variable finish : boolean;
+    variable do_bits : boolean;
   begin
     if Clk'event and Clk = '1'
     then
       WRint <= '0';
 --      DQ <= "ZZZZZZZZ";
-      RDinvInt <= '1';
+      RDiInt <= '1';
       BDC <= 'Z';
-      BDCprev <= BDC;
       count <= count + x"1";
 
+      do_bits := state = send_bits or state = read_bits;
+
+      -- During a data cycle, do the BDC data.
       case count is
         when x"0" | x"1" | x"2" | x"3" =>
-          if state = send_bits or state = read_bits then
+          if do_bits then
             BDC <= '0';
           end if;
         when x"4" | x"5" | x"6" | x"7" | x"8" | x"9" | x"A" | x"B" | x"C" =>
           if state = send_bits then
-            BDC <= data(4);
+            BDC <= data(3);
           end if;
         when others =>
           if state = send_bits then
@@ -69,53 +73,87 @@ begin
           end if;
       end case;
 
+      -- Maintain the bit number.  Doing this on count=0 means counthi is
+      -- correct if we stop a break on the same cycle as counthi increments.
+      if do_bits or state = long_break or state = read_break then
+        if count = x"0" then
+          counthi <= counthi + x"01";
+        end if;
+      end if;
+
+      -- During a break, maintain the 6 bit counter, BDC data, and switch
+      -- to waiting for break reply.
       if state = long_break then
-        if count = x"F" and data = "10000" then
+        if count = x"F" and counthi(5 downto 0) = "011111" then
           BDC <= '1';
-        else
+        elsif counthi(5) = '0' then
           BDC <= '0';
+        else
+          BDC <= 'Z';
+        end if;
+
+        if counthi(5) = '1' and BDC = '0' then
+          count <= x"0";
+          counthi <= x"FF";
+          state <= read_break;
+        end if;
       end if;
 
-      if count = x"A" then
-        case state is
-          when read_bits  =>  data <= data(3 downto 0) & BDC;
-          when send_bits  =>  data <= data(3 downto 0) & '0';
-          when long_break =>  data <= data(3 downto 0) & (data(4) xor data(2));
-          when others     =>  data <= "XXXXX";
-        end case;
+      -- In read break, we're waiting for a BDC=1; also breaks time out.
+      if (state = read_break and BDC = '1')
+        or ((state = long_break or state = read_break)
+            and count = x"F" and counthi(5 downto 0) = "111111") then
+        data <= count;
+        state <= acknowledge;
       end if;
 
-      if count = x"C" and state = read_bits and data(4) = '1' then
+      -- BDC sampling cycle.
+      if count = x"A" and do_bits then
+        data <= data(2 downto 0) & BDC;
+      end if;
+
+      -- Check if we are finishing this command & can read another.
+      finish :=
+        (do_bits and counthi(2 downto 0) = "11")
+        or state = acknowledge
+        or state = idle;
+
+      -- Send data in at the end of each command.
+      if count = x"C" and finish and state /= idle then
         WRint <= '1';
 --        DQ <= x"4" & data(3 downto 0);
       end if;
 
-      finish :=
-        (state = send_bits and data = "10000")
-        or (state = read_bits and data(4) = '1')
-        or (state = long_break and data = "10000")
-        or state = idle;
-
-      if count = x"E" and finish and RXFinv = '0' then
-        RDinvInt <= '0';
+      -- Ask for next command if we want data and its available.
+      if count = x"E" and finish and RXFi = '0' then
+        RDiInt <= '0';
       end if;
 
-      if count = x"F" then
-        if RDinvInt /= '0' then
+      -- Process command, or go to idle if no command.
+      if count = x"F" and finish then
+        if RDiInt /= '0' then
           state <= idle;
-          data <= "XXXXX";
+          data <= data;
         elsif DQ(7 downto 4) = x"4" then
           state <= send_bits;
-          data <= DQ(3 downto 0) & '1';
+          counthi <= x"FF";
+          data <= DQ(3 downto 0);
         elsif DQ(7 downto 4) = x"6" then
           state <= read_bits;
-          data <= "00001";
+          counthi <= x"FF";
+          data <= "XXXX";
         elsif DQ(7 downto 0) = x"21" then
           state <= long_break;
-          data <= "10000";
+          counthi <= x"FF";
+        elsif DQ(7 downto 0) = x"30" then
+          state <= acknowledge;
+          data <= counthi(3 downto 0);
+        elsif DQ(7 downto 0) = x"21" then
+          state <= long_break;
+          data <= counthi(7 downto 4);
         else
           state <= idle;
-          data <= "XXXXX";
+          data <= "XXXX";
         end if;
       end if;
     end if;
