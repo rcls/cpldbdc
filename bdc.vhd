d85b557f9d196421f72380403d8a8b63f9faab4b
Ralph Loader <suckfish@ihug.co.nz>
2010-05-23 15:12:21 +1200

    Attempt to fix syncs. Simplify BDC drive.

--- bdc.vhd
+++ bdc.vhd
@@ -20,10 +20,11 @@ end bdc;
 
 architecture Behavioral of bdc is
   signal count : std_logic_vector (3 downto 0) := x"0";
-  signal counthi : std_logic_vector (4 downto 0) := "00000";
+  signal counthi : std_logic_vector (3 downto 0) := "0000";
   signal data : std_logic_vector (3 downto 0) := "0000";
 
-  type state_t is (ack, idle, send_bits, read_bits, sync_init, sync_low,
+  type state_t is (ack, idle, send_bits, read_bits,
+                   sync_init, sync_wait,
                    read_command, write_result);
 
   signal state : state_t := idle;
@@ -32,8 +33,12 @@ architecture Behavioral of bdc is
 
   signal clkdiv : std_logic_vector(1 downto 0) := "00";
   signal Clk_main : std_logic := '0';
+  signal lastBDC : std_logic := '0';
 
-  constant STOP : std_logic_vector(4 downto 0) := "10011";
+  signal BDCdata : std_logic := '0';
+  signal BDCout : boolean := false;
+
+  constant STOP : std_logic_vector(3 downto 0) := "0011";
   -- 0,1,2,3 - send start pulse.
   -- 4 - drive high for 1.
   -- 10 - sample.
@@ -47,7 +52,9 @@ begin
   RDi <= '0' when state = read_command else '1';
 
   WR <= '1' when state = write_result else '0';
-  DQ <= counthi(3 downto 0) & data when state = write_result else "ZZZZZZZZ";
+  DQ <= counthi & data when state = write_result else "ZZZZZZZZ";
+
+  BDC <= BDCdata when BDCout else 'Z';
 
   process (Clk)
   begin
@@ -65,26 +72,22 @@ begin
     variable do_bits : boolean;
   begin
     if Clk_main'event then
-      BDC <= 'Z';
       count <= count + x"1";
 
       do_bits := state = send_bits or state = read_bits;
 
-      -- During a data cycle, do the BDC data.
-      case count is
-        when x"0" | x"1" | x"2" | x"3" =>
-          if do_bits then
-            BDC <= '0';
-          end if;
-        when x"4" | x"5" | x"6" | x"7" | x"8" | x"9" | x"A" | x"B" | x"C" =>
-          if state = send_bits then
-            BDC <= data(3);
-          end if;
-        when others =>
-          if state = send_bits then
-            BDC <= '1';
-          end if;
-      end case;
+      -- During a data cycle, do the BDC data.  During sync, drive low
+      -- with one cycle speed up.
+      if count = x"0" and (do_bits or state = sync_init) then
+        BDCdata <= '0';
+      elsif count = x"4" and state = send_bits then
+        BDCdata <= data(3);
+      elsif count = x"D" and state = send_bits then
+        BDCdata <= '1';
+      end if;
+
+      BDCout <=  state = send_bits or state = sync_init or
+        (state = read_bits and count(3 downto 2) = "00");
 
       -- Maintain the bit number.  Doing this on count=0 means counthi is
       -- correct if we finish a sync on the same clock as counthi increments.
@@ -92,25 +95,28 @@ begin
         counthi <= counthi + '1';
       end if;
 
-      -- Send sync, with a one-clock speedup.
-      if state = sync_init and counthi(4) = '0' then
-        if counthi(3 downto 0) = "1111" and count = x"F" then
-          BDC <= '1';
-        else
-          BDC <= '0';
-        end if;
+      -- At the end of sync_init, transfer to sync_wait, do the BDC speed-up
+      -- pulse, and set-up lastBDC to stop false triggering on the first
+      -- cycle of sync_wait.
+      if state = sync_init and counthi = x"F" and count = x"F" then
+        state <= sync_wait;
+        BDCdata <= '1';
+        lastBDC <= '1';
+      else
+        lastBDC <= BDC;
       end if;
 
-      -- In read sync, we're waiting for a BDC 0-to-1 transition.
-      if state = sync_init and counthi(4) = '1' and BDC = '0' then
-        state <= sync_low;
-      end if;
-      if (state = sync_low and BDC = '1')
-        or ((state = sync_low or state = sync_init)
-            and counthi = "11111" and count = "1111") then
-        data <= count;
-        state <= ack;
---        state <= sync_init;
+      -- In read sync, we're waiting for a BDC 0-to-1 transition & we also
+      -- time out.
+      if state = sync_wait then
+        if (lastBDC = '0' and BDC = '1') or (counthi = x"F" and count = x"F")
+        then
+          data <= count;
+          state <= ack;
+--        if counthi(3) = '0' then
+--          clkspeed <= clkspeed - 1;
+--        end if;
+        end if;
       end if;
 
       -- BDC sampling cycle.
@@ -132,12 +138,12 @@ begin
       if state = write_result then
         state <= idle;
         data <= "XXXX";
-        counthi <= "XXXXX";
+        counthi <= "XXXX";
       end if;
       if state = read_command then
         state <= idle;
         data <= "XXXX";
-        counthi <= "XXXXX";
+        counthi <= "XXXX";
         if DQ(7 downto 4) = x"4" then
           state <= send_bits;
           data <= DQ(3 downto 0);
@@ -147,7 +153,7 @@ begin
           counthi <= STOP - 4;
         elsif DQ = x"21" then -- '!'
           state <= sync_init;
-          counthi <= "11111";
+          counthi <= "1111";
         elsif DQ(7 downto 2) = "001100" then -- 0,1,2,3
           clkspeed <= DQ(1 downto 0);
         elsif DQ(7 downto 0) = x"22" then -- '"'
